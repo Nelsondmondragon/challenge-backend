fee2bfed6b12ebf686e2ba5401c4d95ad747ca15
Nelson Cortes <nelsondmondragon@gmail.com>
2021-08-27 18:51:45 -0500

    Porject basis

---
+++ src/main/java/com/nedacort/challengespringbackend/ChallengeSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nedacort.challengespringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeSpringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeSpringBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nedacort/challengespringbackend/ChallengeSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nedacort.challengespringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeSpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
