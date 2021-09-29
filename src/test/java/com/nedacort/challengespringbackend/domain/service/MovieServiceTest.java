e5b6f7a3a2ef00478e26d626416707c36b7a7d4b
Nelson Cortes <nelsondmondragon@gmail.com>
2021-09-29 18:07:13 -0500

    Testing MovieService completed

--- src/test/java/com/nedacort/challengespringbackend/domain/service/MovieServiceTest.java
+++ src/test/java/com/nedacort/challengespringbackend/domain/service/MovieServiceTest.java
@@ -1,57 +1,112 @@
 package com.nedacort.challengespringbackend.domain.service;
 
-import org.junit.jupiter.api.Disabled;
+import com.nedacort.challengespringbackend.domain.MovieDto;
+import com.nedacort.challengespringbackend.domain.repository.MovieDtoLimitedRepository;
+import com.nedacort.challengespringbackend.domain.repository.MovieDtoRepository;
+import com.nedacort.challengespringbackend.domain.repository.MoviePersonageDtoRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+
+@ExtendWith(MockitoExtension.class)
 class MovieServiceTest {
+
+
+    @Mock
+    private MovieDtoLimitedRepository movieDtoLimitedRepository;
+
+    @Mock
+    private MoviePersonageDtoRepository moviePersonageDtoRepository;
+
+    @Mock
+    private MovieDtoRepository movieDtoRepository;
+
+    @InjectMocks
+    private MovieService movieService;
+
+
+    @Captor
+    private ArgumentCaptor<MovieDto> argumentCaptor;
 
     @Test
     void get_all_movie_and_personage() {
-
-
+        movieService.getAllMovieAndPersonage();
+        verify(moviePersonageDtoRepository).findAllMoviesAndPersonage();
     }
 
     @Test
-    @Disabled
-    void getAllLimited() {
+    void get_all_movies_by_image_tittle_and_created_date() {
+        movieService.getAllLimited();
+        verify(movieDtoLimitedRepository).getAllLimited();
     }
 
     @Test
-
-    @Disabled
-    void getById() {
+    void get_movie_by_id() {
+        movieService.getById(1);
+        verify(movieDtoRepository).getById(1);
     }
 
     @Test
-
-    @Disabled
-    void getByName() {
+    void get_movie_by_name() {
+        movieService.getByName("It");
+        verify(movieDtoRepository).getByName("It");
     }
 
     @Test
-    @Disabled
-    void getByIdGenre() {
+    void get_movie_by_id_genre() {
+        movieService.getByIdGenre(1);
+        verify(movieDtoRepository).getByIdGenre(1);
     }
 
     @Test
-    @Disabled
-    void findAllByCreationDateDesc() {
+    void get_all_movies_by_create_date_sorted_desc() {
+        movieService.findAllByCreationDateDesc();
+        verify(movieDtoRepository).findAllByCreationDateDesc();
     }
 
     @Test
-    @Disabled
-    void findAllByCreationDateAsc() {
+    void get_all_movies_by_create_date_sorted_asc() {
+        movieService.findAllByCreationDateAsc();
+        verify(movieDtoRepository).findAllByCreationDateAsc();
     }
 
     @Test
-    @Disabled
     void save() {
+        MovieDto movieDto = MovieDto.builder()
+                .id(1)
+                .tittle("It")
+                .creationDate(LocalDateTime.now())
+                .idGenus(1)
+                .idQualification(1)
+                .build();
+        movieService.save(movieDto);
+        argumentCaptor = ArgumentCaptor.forClass(MovieDto.class);
+        verify(movieDtoRepository).save(argumentCaptor.capture());
+        assertThat(movieDto).isEqualTo(argumentCaptor.getValue());
+
     }
 
     @Test
-    @Disabled
     void delete() {
+        MovieDto movieDto = MovieDto.builder()
+                .id(1)
+                .tittle("It")
+                .creationDate(LocalDateTime.now())
+                .idGenus(1)
+                .idQualification(1)
+                .build();
+        Mockito.when(movieService.getById(movieDto.getId()))
+                .thenReturn(Optional.of(movieDto));
+        movieService.delete(movieDto.getId());
+        verify(movieDtoRepository).delete(movieDto.getId());
     }
 }
